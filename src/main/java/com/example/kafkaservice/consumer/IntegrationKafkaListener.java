4eb2ba8908609e919a56b6f63b6fbe26562a1b48
Артем <av.10.000@yandex.ru>
2026-04-18 13:23:18 +0300

    update yaml

--- src/main/java/com/example/kafkaservice/consumer/IntegrationKafkaListener.java
+++ src/main/java/com/example/kafkaservice/consumer/IntegrationKafkaListener.java
@@ -20,6 +20,5 @@ public class IntegrationKafkaListener {
     @KafkaListener(topics = "${app.kafka.topic}")
     public void listen(ConsumerRecord<String, String> record) {
         IntakeResult result = kafkaIntakeService.intake(record);
-        applyOrchestrator.applyByLoadingId(result.loadingId());
     }
 }
